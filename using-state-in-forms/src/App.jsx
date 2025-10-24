45c440890ac573b60e2c951ccb1a0ce8b3f51b02
edwardmartin1 <edwardmartin1@yahoo.com>
2025-10-23 21:27:04 -0500

    first commit

---
+++ using-state-in-forms/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+import UserForm from "./components/UserForm"
+
+function App() {
+
+  return (
+    <div>
+      <h1>Using State in Forms</h1>
+      <UserForm />
+
+    </div>
+  );
+}
+
+export default App;
+
